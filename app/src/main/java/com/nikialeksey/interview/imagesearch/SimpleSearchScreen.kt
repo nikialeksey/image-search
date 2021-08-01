971c18715c7e01391ec2d2928abfb91291adf127
Alexey Nikitin <nikialeksey@gmail.com>
2021-08-01 13:33:42 +0700

    Image open transition

--- app/src/main/java/com/nikialeksey/interview/imagesearch/SimpleSearchScreen.kt
+++ app/src/main/java/com/nikialeksey/interview/imagesearch/SimpleSearchScreen.kt
@@ -1,7 +1,10 @@
 package com.nikialeksey.interview.imagesearch
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import com.bumptech.glide.RequestManager
 import com.nikialeksey.interview.imagesearch.images.Image
 import com.nikialeksey.interview.imagesearch.images.Images
@@ -19,11 +22,17 @@ class SimpleSearchScreen(
 
     override fun navigation(): Navigation {
         return object: Navigation {
-            override fun openImage(navController: NavController, image: Image) {
+            override fun openImage(navController: NavController, image: Image, shared: View) {
                 val args = Bundle()
                 args.putString("thumbnailUrl", image.thumbnailUrl())
-                args.putString("url", image.largeUrl())
-                navController.navigate(R.id.action_show_image, args)
+                navController.navigate(
+                    R.id.action_show_image,
+                    args,
+                    null,
+                    FragmentNavigatorExtras(
+                        shared to image.thumbnailUrl()
+                    )
+                )
             }
         }
     }
