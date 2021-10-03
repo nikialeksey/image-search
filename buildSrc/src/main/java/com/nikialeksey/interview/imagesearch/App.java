93f67a972e14982058306d4243fe094d9ee7e7b6
Alexey Nikitin <nikialeksey@gmail.com>
2021-10-03 19:49:00 +0700

    Support Android 12

--- buildSrc/src/main/java/com/nikialeksey/interview/imagesearch/App.java
+++ buildSrc/src/main/java/com/nikialeksey/interview/imagesearch/App.java
@@ -2,10 +2,10 @@ package com.nikialeksey.interview.imagesearch;
 
 public class App {
     public static final String APPLICATION_ID = "com.nikialeksey.interview.imagesearch";
-    public static final String BUILD_TOOLS_VERSION = "30.0.2";
+    public static final String BUILD_TOOLS_VERSION = "31.0.0";
     public static final int MIN_SDK_VERSION = 24;
-    public static final int TARGET_SDK_VERSION = 30;
-    public static final int COMPILE_SDK_VERSION = 30;
+    public static final int TARGET_SDK_VERSION = 31;
+    public static final int COMPILE_SDK_VERSION = 31;
     public static final int VERSION_CODE = 1;
     public static final String VERSION_NAME = "0.0.1";
 }
